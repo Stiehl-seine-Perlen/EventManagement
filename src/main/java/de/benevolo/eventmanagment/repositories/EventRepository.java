857b5045bdb59b6cd0ce33c6f5fc665759c33fc0
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-05-03 19:52:14 +0200

    🚨EventRepository @ApplicationScoped

--- src/main/java/de/benevolo/eventmanagment/repositories/EventRepository.java
+++ src/main/java/de/benevolo/eventmanagment/repositories/EventRepository.java
@@ -3,8 +3,10 @@ package de.benevolo.eventmanagment.repositories;
 import de.benevolo.entities.events.Event;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 
+@ApplicationScoped
 public class EventRepository implements PanacheRepository<Event> {
 
 
