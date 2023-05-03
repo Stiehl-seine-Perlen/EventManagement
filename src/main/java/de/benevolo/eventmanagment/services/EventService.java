9c74294268266d16fcfa51a5afe751ee2230e5c4
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-05-03 19:30:28 +0200

    🚧added EventService.java

---
+++ src/main/java/de/benevolo/eventmanagment/services/EventService.java
@@ -0,0 +1,44 @@
+package de.benevolo.eventmanagment.services;
+
+import com.github.dockerjava.api.exception.NotAcceptableException;
+import de.benevolo.entities.events.Event;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import de.benevolo.eventmanagment.repositories.EventRepository;
+
+import java.util.List;
+
+@ApplicationScoped
+public class EventService {
+
+   @Inject EventRepository eventRepository;
+
+   public Event getEventById(Long id){
+       return eventRepository.findById(id);
+   }
+
+   public List<Event> getAllEvents(){
+       return eventRepository.listAll();
+   }
+
+   public List<Event> getAllEventsOfAssociation(Long associationId){
+       return eventRepository.getAllEventsOfAssociation(associationId);
+   }
+
+   @Transactional
+   public Event persistEvent(Event event){
+       try {
+           eventRepository.persist(event);
+           return event;
+       } catch (Exception e) {
+           throw new NotAcceptableException("Could Not Persist Event with name" + event.getEventName());
+       }
+   }
+
+   public Event createEvent(Event event){
+       return this.persistEvent(event);
+   }
+}
