7ba1e869fbde94ade955de08e609aac28cdc9ab4
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-05-03 19:51:35 +0200

    🚧added EventResource.java

---
+++ src/main/java/de/benevolo/eventmanagment/controller/EventResource.java
@@ -0,0 +1,45 @@
+package de.benevolo.eventmanagment.controller;
+
+import com.oracle.svm.core.annotate.Inject;
+import de.benevolo.entities.events.Event;
+import de.benevolo.eventmanagment.services.EventService;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/event")
+public class EventResource {
+
+    @Inject
+    EventService eventService;
+
+    @GET
+    @Path("{eventId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Event getEventById(@PathParam("eventId") Long id){
+        return eventService.getEventById(id);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Event> getAllEvents(){
+        return eventService.getAllEvents();
+    }
+
+    @GET
+    @Path("{associationId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Event> getAllEventsForAssociation(@PathParam("associationId") Long associationId){
+        return eventService.getAllEventsOfAssociation(associationId);
+    }
+
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Event createEvent(Event event){
+        return eventService.persistEvent(event);
+    }
+
+}
